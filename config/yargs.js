f975a1f6908b93d51461954e0990a75721cf5114
Roberto González <rgonzalez@creasys.cl>
2018-08-10 11:55:14 -0400

    Primer Commit

---
+++ config/yargs.js
@@ -0,0 +1,28 @@
+const descripcion = {
+    alias: 'd',
+    demand: true,
+    desc: 'Descripción de la tarea por hacer'
+}
+
+const completado = {
+    alias: 'c',
+    default: true,
+    desc: 'Marca como completado o pendiente la tarea'
+}
+
+const { argv } = require('yargs')
+    .command('crear', 'Crear un elemento por hacer', {
+        descripcion
+    })
+    .command('actualizar', 'Actualizar el estado completado de una tarea', {
+        descripcion,
+        completado
+    })
+    .command('borrar', 'Borrar una tarea del listado', {
+        descripcion
+    })
+    .help()
+
+module.exports = {
+    argv
+}
